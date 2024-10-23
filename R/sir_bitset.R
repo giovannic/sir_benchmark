d95b07df231f429fff3ba40f387832d567ccb464
Giovanni Charles <giovanni.charles@gmail.com>
2024-10-23 10:36:46 +0100

    Make sensible plots for each method:
     * Fix all methods
     * Add plotting script
     * Reorder parameter sampling script
     * Render the initial state for extra validation

--- R/sir_bitset.R
+++ R/sir_bitset.R
@@ -8,18 +8,19 @@ output_file <- args[3]
 # Read parameters from file
 parameters <- read.csv(parameters, header = FALSE, col.names=c("I0","R0","gamma"))
 
+dt <- 0.1
+tmax <- 100
+steps <- tmax/dt
+aggregate_health_states <- matrix(0, nrow = steps * nrow(parameters), ncol = 5)
+
 # Loop over each run
 for (i in 1:nrow(parameters)) {
     # Initialise model arguments
     I0 <- round(parameters[[i,'I0']] * N)
-    R0 <- round(parameters[[i,'R0']] * N)
-    S0 <- N - I0 - R0
-    dt <- 0.1
-    tmax <- 100
-    steps <- tmax/dt
+    R0 <- parameters[[i,'R0']]
+    S0 <- N - I0
     gamma <- parameters[i,]$gamma
     beta <- R0 * gamma
-    aggregate_health_states <- matrix(0, nrow = steps, ncol = 5)
     S = individual::Bitset$new(N)$not()
     I = individual::Bitset$new(N)$insert(sample.int(n = N,size = I0))
     R = individual::Bitset$new(N)
@@ -27,19 +28,19 @@ for (i in 1:nrow(parameters)) {
 
 	# Loop over each timestep
 	for (t in 1:steps) {
+        # Save aggregate health states
+        aggregate_health_states[(i-1)*steps + t,] <- c(i,t,S$size(),I$size(),R$size())
+
         # calculate the force of infection
         foi <- I$size() * beta / N
 
-        new_infections = S$copy()$sample(min(foi, 1))
+        new_infections = S$copy()$sample(min(foi * dt, 1))
         I$or(new_infections)
         S$and(new_infections$not())
 
-        new_recoveries = I$copy()$sample(gamma)
+        new_recoveries = I$copy()$sample(gamma * dt)
         R$or(new_recoveries)
         I$and(new_recoveries$not())
-
-        # Save aggregate health states
-        aggregate_health_states[t,] <- c(i,t,S$size(),I$size(),R$size())
 	}
 }
 
--- README.md
+++ README.md
@@ -15,12 +15,18 @@ or with different population counts, first arg, and batch size, second arg
 docker run -it benchmark /bin/bash ./benchmark.sh 10,100,1000 1000"
 ```
 
+To run plots to validate model outputs:
+
+```bash
+docker run -it benchmark /bin/bash ./plot.sh"
+```
+
 ## TODO
 
- - [] Review implementations for correctness
- - [] Sweep through batch sizes
- - [] JAX GPU version
- - [] Competing hazards versions
+ - [x] Review implementations for correctness
  - [] Household and network SIR versions
+ - [] Competing hazards versions
  - [] Agents.jl version
+ - [] JAX GPU version
+ - [] Sweep through batch sizes
  - [] Documentation
